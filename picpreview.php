5f0b41e944f4c057ae0e675153a30196e0abdc5b
MahendraGayatri <mahendragayatri@gmail.com>
2016-07-17 17:41:03 -0500

    Updates with 2016 contest images

--- picpreview.php
+++ picpreview.php
@@ -2,13 +2,44 @@
 <html xmlns="http://www.w3.org/1999/xhtml">
 <head>
 	<title>Goshala Images</title>
+	<link href="css/style.css" rel="stylesheet" type="text/css" />
+<link rel="stylesheet" type="text/css" href="css/coin-slider.css" />
+<script type="text/javascript" src="js/jquery-1.4.2.min.js"></script>
+<script type="text/javascript" src="js/script.js"></script>
+
 	<link rel="stylesheet" href="css/bootstrap.min.css">
 <link rel="stylesheet" href="css/blueimp-gallery.min.css">
 <link rel="stylesheet" href="css/bootstrap-image-gallery.min.css">
 </head>
 
 <body>
-
+<div class="main">
+  <div class="header">
+    <div class="header_resize">
+      <div class="searchform">
+		501(c) Tax-exempt Nonprofit Organization<BR>	
+        <form action="https://www.paypal.com/cgi-bin/webscr" method="post" target="_top"><input name="cmd" type="hidden" value="_s-xclick"> <input name="hosted_button_id" type="hidden" value="HV3GLZ8HLE322"> <input alt="PayPal - The safer, easier way to pay online!" name="submit" src="https://www.paypalobjects.com/en_US/i/btn/btn_donateCC_LG.gif" type="image"> <img src="https://www.paypalobjects.com/en_US/i/scr/pixel.gif" alt="" width="1" height="1" border="0"></form>
+      </div>
+      <div class="">
+        <h1 font-family='Georgia, serif' font-size='20' ><a href="index.html"><img src="images/Goshalaicon.png" /><span style="width: 258px; height: 53px; top: -30px; left: -1px;" > Goshala Iowa</span></a></h1>
+      </div>
+      <div class="clr"></div>
+      <div class="menu_nav">
+        <ul>
+          <li><a href="index.html"><span>Home</span></a></li>
+          <li><a href="support.html"><span>Our Projects</span></a></li>
+          <li><a href="about.html"><span>About Cows</span></a></li>
+          <li><a href="blog.html"><span>Events</span></a></li>
+          <li><a href="contact.html"><span>Contact Us</span></a></li>
+        </ul>
+      </div>      
+    </div>
+  </div>
+  <div class="content">
+    <div class="content_resize">
+      <div class="mainbar">
+        <div class="article">
+          <h2><span>Cows</span> we saved in the past</h2>
 <!-- The Bootstrap Image Gallery lightbox, should be a child element of the document body -->
 <div id="blueimp-gallery" class="blueimp-gallery">
     <!-- The container for the modal slides -->
@@ -57,8 +88,9 @@
     </a>
     <a href="images/savedcows/cow3.jpg" title="Shreeya" data-gallery>
        <img src="images/savedcows/cow3_thumbnail.jpg" alt="Shreeya">
-    </a><br/>
-    <strong>2016 Kids Picture Contest Drawings</strong><br/>
+    </a><br/><br/>
+    <h2><span>2016 Kids Picture Contest </span> Drawings</h2>
+    <br/>
     <a href="images/contest/2016_1_1.jpg" title="Raghav Gayatri, 3 yrs" data-gallery>
         <img src="images/contest/2016_1_1_tn.jpg" alt="Raghav">
     </a>
@@ -77,6 +109,24 @@
     <a href="images/contest/2016_3_3.jpg" title="Sneha Hegde, 9 yrs" data-gallery>
        <img src="images/contest/2016_3_3_tn.jpg" alt="Sneha">
     </a>
+    <a href="images/contest/2016_4_1.jpg" title="Anaya Patil, 5 yrs" data-gallery>
+        <img src="images/contest/2016_4_1_tn.jpg" alt="Anaya">
+    </a>
 </div> 
+<br/>
+<div id="fb-root"></div>
+<script>(function(d, s, id) {
+  var js, fjs = d.getElementsByTagName(s)[0];
+  if (d.getElementById(id)) return;
+  js = d.createElement(s); js.id = id;
+  js.src = "//connect.facebook.net/en_US/sdk.js#xfbml=1&version=v2.7";
+  fjs.parentNode.insertBefore(js, fjs);
+}(document, 'script', 'facebook-jssdk'));</script>
+<div class="fb-like" data-href="http://goshalaiowa.org/" data-width="100" data-layout="button_count" data-action="like" data-size="large" data-show-faces="true" data-share="true"></div>
+</div>
+  </div></div>
+  </div>
+  </div>
+  
 </body>
 </html>
